ee877b52d8ae015bc9241fb4d9d5eb9aa8617ee2
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-29 16:28:24 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.public")
+
+@section("title","homepage")
+@section("home")
+    <h1>ciao</h1>
+@endsection
---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    <title>@yield("title")</title>
+</head>
+<body>
+@include("layouts.partials.header")
+<main>
+
+@yield("home")
+</main>
+   @include("layouts.partials.footer") 
+</body>
+</html>
